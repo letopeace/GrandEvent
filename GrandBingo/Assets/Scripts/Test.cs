6efa99d571905be89066c299cbefe8e0bc8472ec
letopeace <dumanaskerbek@gmail.com>
2025-05-23 16:14:19 +0500

    Scene

--- GrandBingo/Assets/Scripts/Test.cs
+++ GrandBingo/Assets/Scripts/Test.cs
@@ -3,10 +3,44 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    private Revolver revolver = new Revolver(2);
+    private Revolver revolver;
 
     void Update()
     {
-        
-    }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Show(1);
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			Show(2);
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			Show(3);
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha4))
+		{
+			Show(4);
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha5))
+		{
+			Show(5);
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha6))
+		{
+			Show(6);
+		}
+	}
+
+    private void Show(int count)
+    {
+		revolver = new Revolver(count);
+        bool[] drum = revolver.Show();
+
+		foreach (bool drumItem in drum)
+		{
+			Debug.Log(drumItem);
+		}
+	}
 }
